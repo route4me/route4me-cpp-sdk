53b258f5e7e766dbb91950e9abde239be2e759bf
Alex <alexander.stoyakin@gmail.com>
2016-11-07 17:43:07 +0200

    Getting all routes example

--- test/GetMultipleRoutes.cpp
+++ test/GetMultipleRoutes.cpp
@@ -12,12 +12,6 @@ using namespace std;
 
 int main()
 {
-   /* ifstream inf("addr.json");
-    if(!inf.is_open())
-    {
-        cout << "cannot find a file with addresses" << endl;
-        return -1;
-    }*/
     // global init
     CRoute4Me::init();
     CRoute4Me route(KEY);
@@ -25,13 +19,11 @@ int main()
     int ret = 0;
     if(route.get_multiple_routes() == 0)
     {
-        /*Json::Value addr;
-        inf >> addr;*/
         cout << "All routes:" << endl << Json::StyledWriter().write(route.get_json_resp()) << endl;
     }
     else
     {
-        cout << "Err: " << route.get_err_code() << ": " << route.get_err_msg() << endl;
+        cout << "Error: " << route.get_err_code() << ": " << route.get_err_msg() << endl;
         ret = -1;
     }
     // global cleanup
