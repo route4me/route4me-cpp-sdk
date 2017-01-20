497f6fad181c78c4b07ed0069a19a07851e5f807
Alex <alexander.stoyakin@gmail.com>
2017-01-20 13:59:22 +0200

    Cleanup

--- src/route4me.cpp
+++ src/route4me.cpp
@@ -8,7 +8,6 @@
 #include <cstdlib>
 #include <cstring>
 #include <curl/curl.h>
-#include <sstream>
 #include "../include/route4me.h"
 
 ///////////////////////////////////////////////////////////////////////////////
